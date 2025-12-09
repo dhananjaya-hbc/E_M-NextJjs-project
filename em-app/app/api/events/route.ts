dc8944aa09ee912d19197278d0069464d3d725bd
Dhananjaya Chamod <dhananjayahbc.23@uom.lk>
2025-12-09 19:06:54 +0530

    added files and did some changes

--- em-app/app/api/events/route.ts
+++ em-app/app/api/events/route.ts
@@ -1,59 +1,64 @@
-import { NextRequest, NextResponse } from "next/server";
-import { v2 as cloudinary  } from 'cloudinary'; 
+import {NextRequest, NextResponse} from "next/server";
+import { v2 as cloudinary } from 'cloudinary';
 
-import connectToDatabase  from "@/lib/mongoose";
-import Event from "@/database/event.model";
+import connectDB from "@/lib/mongodb";
+import Event from '@/database/event.model';
 
 export async function POST(req: NextRequest) {
     try {
-        await  connectToDatabase();
+        await connectDB();
+
         const formData = await req.formData();
 
         let event;
 
-        try{
-            event   =   Object.fromEntries(formData.entries());
-        }catch{
-            return NextResponse.json({message:'Invalid Json format data'},{status:400});
+        try {
+            event = Object.fromEntries(formData.entries());
+        } catch (e) {
+            return NextResponse.json({ message: 'Invalid JSON data format'}, { status: 400 })
         }
 
         const file = formData.get('image') as File;
-        if (!file) {
-            return NextResponse.json({message:'Image file is required'},{status:400});
-        }
+
+        if(!file) return NextResponse.json({ message: 'Image file is required'}, { status: 400 })
+
+        const tags = JSON.parse(formData.get('tags') as string);
+        const agenda = JSON.parse(formData.get('agenda') as string);
 
         const arrayBuffer = await file.arrayBuffer();
         const buffer = Buffer.from(arrayBuffer);
 
-        const uploadResult = await new Promise<{ secure_url: string }>((resolve, reject) => {
-            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'events' }, (error, result) => {
-                if (error) return reject(error);
-                resolve(result as { secure_url: string });
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevEvent' }, (error, results) => {
+                if(error) return reject(error);
 
+                resolve(results);
             }).end(buffer);
-            
         });
 
-        event.image=(uploadResult as { secure_url: string }).secure_url;
+        event.image = (uploadResult as { secure_url: string }).secure_url;
 
-        const createdEvent = await Event.create(event);
-        return NextResponse.json({message:'event created successfully',event:createdEvent},{status:201});
-    
-    }catch (error) {
-        console.error(error);
-        return NextResponse.json({message:'event creation failed',error: error  instanceof Error ? error.message :'Unknown error'});
- }   
-    
+        const createdEvent = await Event.create({
+            ...event,
+            tags: tags,
+            agenda: agenda,
+        });
+
+        return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 });
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'}, { status: 500 })
+    }
 }
 
-export async function GET(req: NextRequest, res: NextResponse  ) {
+export async function GET() {
     try {
-        await connectToDatabase();
+        await connectDB();
 
-        const events = await Event.find().sort({createdAt: -1});
-        return NextResponse.json({message:'events fetched successfully', events},{status:200});
-    }catch (error) {
-        return NextResponse.json({message:'Failed to fetch events',error: error}, {status:500});
+        const events = await Event.find().sort({ createdAt: -1 });
+
+        return NextResponse.json({ message: 'Events fetched successfully', events }, { status: 200 });
+    } catch (e) {
+        return NextResponse.json({ message: 'Event fetching failed', error: e }, { status: 500 });
     }
-
 }
